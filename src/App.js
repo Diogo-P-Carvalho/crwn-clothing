0cbc3d12a28beec53640bb3c5d0d1010afe2849d
Diogo-P-Carvalho <dpcarvalho86@gmail.com>
2023-02-09 22:24:25 +0000

    setting up categories

--- src/App.js
+++ src/App.js
@@ -1,41 +1,39 @@
 export default function App() {
-    return (
-        <div className="categories-container">
-            <div className="category-container">
-                {/* <img /> */}
-                <div className="category-body-container">
-                    <h2>Hats</h2>
-                    <p>Shop Now</p>
-                </div>
-            </div>
-            <div className="category-container">
-                {/* <img /> */}
-                <div className="category-body-container">
-                    <h2>Jackets</h2>
-                    <p>Shop Now</p>
-                </div>
-            </div>
-            <div className="category-container">
-                {/* <img /> */}
-                <div className="category-body-container">
-                    <h2>Sneackers</h2>
-                    <p>Shop Now</p>
-                </div>
-            </div>
-            <div className="category-container">
-                {/* <img /> */}
-                <div className="category-body-container">
-                    <h2>Womens</h2>
-                    <p>Shop Now</p>
-                </div>
-            </div>
-            <div className="category-container">
-                {/* <img /> */}
-                <div className="category-body-container">
-                    <h2>Mens</h2>
-                    <p>Shop Now</p>
-                </div>
-            </div>
+  const categories = [
+    {
+      id: 1,
+      title: "Hats",
+    },
+    {
+      id: 2,
+      title: "Jackets",
+    },
+    {
+      id: 1,
+      title: "Sneackers",
+    },
+    {
+      id: 1,
+      title: "Womens",
+    },
+    {
+      id: 1,
+      title: "Mens",
+    },
+  ];
+
+  return (
+    <div className="categories-container">
+      {categories.map(({ title }) => (
+        <div className="category-container">
+          <div className="background-image"></div>
+
+          <div className="category-body-container">
+            <h2>{title}</h2>
+            <p>Shop Now</p>
+          </div>
         </div>
-    );
+      ))}
+    </div>
+  );
 }
