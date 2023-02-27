390aad8a7b8d176cabf734502e019b06f86bd183
Diogo-P-Carvalho <dpcarvalho86@gmail.com>
2023-02-27 21:42:08 +0000

    added shop page and product card component

--- src/routes/navigation/Navigation.jsx
+++ src/routes/navigation/Navigation.jsx
@@ -19,6 +19,9 @@ export default function Navigation() {
           <CrwnLogo className="logo" />
         </Link>
         <div className="nav-links-container">
+          <Link className="nav-link" to="/shop">
+            SHOP
+          </Link>
           {currentUser ? (
             <span className="nav-link" onClick={signOutUser}>
               SIGN OUT
