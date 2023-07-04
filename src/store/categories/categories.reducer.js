b3d615605925d00c33cc0a3030faf72714aafcf9
Diogo-P-Carvalho <dpcarvalho86@gmail.com>
2023-07-04 21:59:40 +0100

    Remove commented code

--- src/store/categories/categories.reducer.js
+++ src/store/categories/categories.reducer.js
@@ -28,18 +28,3 @@ export const { setCategoriesLoading, setCategories, setCategoriesError } =
   categoriesSlice.actions;
 
 export default categoriesSlice.reducer;
-
-// = (state = INITIAL_STATE, action = {}) => {
-//   const { type, payload } = action;
-
-//   switch (type) {
-//     case CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START:
-//       return { ...state, isLoading: true };
-//     case CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_SUCCESS:
-//       return { ...state, categories: payload, isLoading: false };
-//     case CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_FAILED:
-//       return { ...state, error: payload, isLoading: false };
-//     default:
-//       return state;
-//   }
-// };
