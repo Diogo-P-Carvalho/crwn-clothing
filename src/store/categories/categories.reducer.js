b20cf1b664d5bb6ee2730f770909e380c9f8e36f
Diogo-P-Carvalho <dpcarvalho86@gmail.com>
2023-07-04 21:40:32 +0100

    Redux-Toolkit: Add categories createSlice

--- src/store/categories/categories.reducer.js
+++ src/store/categories/categories.reducer.js
@@ -1,4 +1,4 @@
-import { CATEGORIES_ACTION_TYPES } from "./categories.types";
+import { createSlice } from "@reduxjs/toolkit";
 
 const INITIAL_STATE = {
   categories: [],
@@ -6,17 +6,40 @@ const INITIAL_STATE = {
   error: null,
 };
 
-export const categoriesReducer = (state = INITIAL_STATE, action = {}) => {
-  const { type, payload } = action;
+export const categoriesSlice = createSlice({
+  name: "categories",
+  initialState: INITIAL_STATE,
+  reducers: {
+    setCategoriesLoading(state, action) {
+      state.isLoading = action.payload;
+    },
+    setCategories(state, action) {
+      state.isLoading = false;
+      state.categories = action.payload;
+    },
+    setCategoriesError(state, action) {
+      state.isLoading = false;
+      state.error = action.payload;
+    },
+  },
+});
 
-  switch (type) {
-    case CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START:
-      return { ...state, isLoading: true };
-    case CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_SUCCESS:
-      return { ...state, categories: payload, isLoading: false };
-    case CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_FAILED:
-      return { ...state, error: payload, isLoading: false };
-    default:
-      return state;
-  }
-};
+export const { setCategoriesLoading, setCategories, setCategoriesError } =
+  categoriesSlice.actions;
+
+export default categoriesSlice.reducer;
+
+// = (state = INITIAL_STATE, action = {}) => {
+//   const { type, payload } = action;
+
+//   switch (type) {
+//     case CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START:
+//       return { ...state, isLoading: true };
+//     case CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_SUCCESS:
+//       return { ...state, categories: payload, isLoading: false };
+//     case CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_FAILED:
+//       return { ...state, error: payload, isLoading: false };
+//     default:
+//       return state;
+//   }
+// };
