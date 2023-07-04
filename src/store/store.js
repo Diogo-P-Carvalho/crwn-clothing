36818524e9bdaf93e04690ff7f9bf726b41e28ea
Diogo-P-Carvalho <dpcarvalho86@gmail.com>
2023-07-04 22:14:37 +0100

    Redux-Toolkit: Add redux-persist

--- src/store/store.js
+++ src/store/store.js
@@ -1,37 +1,41 @@
-// import { compose, createStore, applyMiddleware } from "redux";
 import { configureStore } from "@reduxjs/toolkit";
-// import { persistStore, persistReducer } from "redux-persist";
-// import storage from "redux-persist/lib/storage";
-// import thunk from "redux-thunk";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import thunk from "redux-thunk";
 import logger from "redux-logger";
 
 import { rootReducer } from "./root-reducer";
 
-// const persistConfig = {
-//   key: "root",
-//   storage,
-//   whitelist: ["cart"],
-// };
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["cart"],
+};
 
-// const persistedReducer = persistReducer(persistConfig, rootReducer);
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const middleWares = [
   process.env.NODE_ENV !== "production" && logger,
-  //thunk,
+  thunk,
 ].filter(Boolean);
 
-// const composeEnhancer =
-//   (process.env.NODE_ENV !== "production" &&
-//     window &&
-//     window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
-//   compose;
-
-// const composedEnhancers = composeEnhancer(applyMiddleware(...middleWares));
-
 export const store = configureStore({
-  reducer: rootReducer,
-  middleware: (getDefaultMeddleware) =>
-    getDefaultMeddleware().concat(middleWares),
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }).concat(middleWares),
 });
 
-// export const persistor = persistStore(store);
+export const persistor = persistStore(store);
